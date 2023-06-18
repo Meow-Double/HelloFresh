3e0f706222036e669e26568ed26e734333d58c68
Meow-Double <slava.pugachev.2015@mail.ru>
2023-06-18 17:27:11 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 const slider = new Swiper(".menu__slider", {
-  freeMode: true,
-  slidesPerView: 3.5,
-  spaceBetween: 20,
-  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // loop: true,
+  pagination: {
+    el: ".menu__slider-pagination",
+  },
   navigation: {
     nextEl: ".menu__slider-arrow--right",
     prevEl: ".menu__slider-arrow--left",
   },
+  breakpoints: {
+    700: {
+      slidesPerView: 1.5,
+      spaceBetween: 10,
+    },
+    840: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    900: {
+      slidesPerView: 2.5,
+      spaceBetween: 15,
+    },
+
+    1070: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3.5,
+      spaceBetween: 20,
+    },
+  },
 });
 
 const accordionBtn = document.querySelectorAll(".questions__item-content");
-const accordionImg = document.querySelectorAll(".question__item-arrow");
+const accordionImg = document.querySelectorAll(".questions__item-arrow");
 
 accordionBtn.forEach((item, index) => {
   item.addEventListener("click", () => {
@@ -30,10 +55,10 @@ const burgerPage = document.querySelector(".burger-page");
 burgerBtn.addEventListener("click", () => {
   burgerPage.classList.toggle("burger-page--active");
   if (burgerPage.classList.contains("burger-page--active")) {
-    console.log("yes")
+    console.log("yes");
     document.body.style.overflow = "hidden";
   } else {
     document.body.style.overflowY = "auto";
-    console.log("no")
+    console.log("no");
   }
 });
